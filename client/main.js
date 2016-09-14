983868b1a8de3398ec1669b566ca80e625254fb6
Oscar Rodriguez <oscarpino711@gmail.com>
2016-09-14 12:03:14 -0500

    add new form

--- client/main.js
+++ client/main.js
@@ -3,18 +3,18 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
+Template.SUMA.onCreated(function helloOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
 });
 
-Template.hello.helpers({
+Template.SUMA.helpers({
   counter() {
     return Template.instance().counter.get();
   },
 });
 
-Template.hello.events({
+Template.SUMA.events({
   'click #SUMA'(event, instance) {
     // increment the counter when button is clicked
     instance.counter.set(instance.counter.get() + 1);
@@ -22,5 +22,23 @@ Template.hello.events({
   'click #RESTA'(event, instance) {
     // increment the counter when button is clicked
     instance.counter.set(instance.counter.get() - 1);
+  }
+});
+
+Template.Formulario.events({
+  	'submit' (event){
+  		event.preventDefault();
+		var first_name = event.target.first_name.value;
+		var last_name = event.target.last_name.value;
+		var password = event.target.password.value;
+		var email = event.target.email.value;
+
+		var person = {
+			name: first_name + " " + last_name,
+			password: password,
+			email: email 
+		}
+		console.log("Person: ", person);
+		event.target.first_name.value = "";
   }
 });
